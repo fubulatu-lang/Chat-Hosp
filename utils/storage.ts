5d55d87466429fb1e016277320639c263ebc55fd
fubulatu-lang <fubulatu@gmail.com>
2026-07-10 01:05:16 +0000

    Create utils/storage.ts

---
+++ utils/storage.ts
@@ -0,0 +1,29 @@
+export const Storage={
+
+set(key:string,value:any){
+
+localStorage.setItem(
+
+key,
+
+JSON.stringify(value)
+
+);
+
+},
+
+get(key:string){
+
+const value=localStorage.getItem(key);
+
+return value?JSON.parse(value):null;
+
+},
+
+remove(key:string){
+
+localStorage.removeItem(key);
+
+}
+
+};
