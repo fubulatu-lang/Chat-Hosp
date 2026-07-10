1dc2f0fd1043e9572f0fc7088c04f367ef59bce9
fubulatu-lang <fubulatu@gmail.com>
2026-07-10 10:41:43 +0000

    Create src/store/index.ts

---
+++ src/store/index.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+
+interface AppState {
+
+    loading: boolean;
+
+    setLoading: (loading: boolean) => void;
+
+}
+
+export const useAppStore = create<AppState>(
+
+    set => ({
+
+        loading: false,
+
+        setLoading: loading =>
+
+            set({
+
+                loading
+
+            })
+
+    })
+
+);
