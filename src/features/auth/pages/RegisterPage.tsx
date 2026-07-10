45492b2b715a8cac487f4ae1df178f658a8bf0db
fubulatu-lang <fubulatu@gmail.com>
2026-07-10 00:49:22 +0000

    Create src/features/auth/pages/RegisterPage.tsx

---
+++ src/features/auth/pages/RegisterPage.tsx
@@ -0,0 +1,17 @@
+import AuthLayout from "../components/AuthLayout";
+
+import RegisterForm from "../components/RegisterForm";
+
+export default function RegisterPage(){
+
+return(
+
+<AuthLayout>
+
+<RegisterForm/>
+
+</AuthLayout>
+
+);
+
+}
