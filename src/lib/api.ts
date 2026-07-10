8c49b03cad55e220b6f24505169d2bd9b8794a6f
fubulatu-lang <fubulatu@gmail.com>
2026-07-10 10:40:51 +0000

    Create src/lib/api.ts

---
+++ src/lib/api.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const api = axios.create({
+
+    baseURL: "/api",
+
+    timeout: 30000,
+
+    headers: {
+
+        "Content-Type": "application/json"
+
+    }
+
+});
+
+api.interceptors.request.use(config => {
+
+    const token = localStorage.getItem("token");
+
+    if (token) {
+
+        config.headers.Authorization = `Bearer ${token}`;
+
+    }
+
+    return config;
+
+});
+
+export default api;
