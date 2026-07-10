a4df855a041853bc581145cfe67b75e6d51cc4c3
fubulatu-lang <fubulatu@gmail.com>
2026-07-10 00:49:01 +0000

    Create src/features/auth/pages/LoginPage.tsx

---
+++ src/features/auth/pages/LoginPage.tsx
@@ -0,0 +1,17 @@
+import AuthLayout from "../components/AuthLayout";
+
+import LoginForm from "../components/LoginForm";
+
+export default function LoginPage(){
+
+return(
+
+<AuthLayout>
+
+<LoginForm/>
+
+</AuthLayout>
+
+);
+
+}
