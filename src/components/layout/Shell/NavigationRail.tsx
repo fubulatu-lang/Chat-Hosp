3806a228b7c05200fea3cb6e514e60cc74f7be98
fubulatu-lang <fubulatu@gmail.com>
2026-07-10 01:10:39 +0000

    Create src/components/layout/Shell/NavigationRail.tsx

---
+++ src/components/layout/Shell/NavigationRail.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+
+export default function NavigationRail() {
+  return (
+    <Box
+      sx={{
+        width: 80,
+        borderRight: 1,
+        borderColor: "divider"
+      }}
+    />
+  );
+}
