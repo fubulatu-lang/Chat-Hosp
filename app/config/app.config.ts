a63254d6dd7e6a55e331bc7d207558265e7a5fd7
fubulatu-lang <fubulatu@gmail.com>
2026-07-10 01:00:27 +0000

    Create app/config/app.config.ts

---
+++ app/config/app.config.ts
@@ -0,0 +1,17 @@
+export const AppConfig = {
+
+    appName: "HRAPIMS",
+
+    version: "1.0.0",
+
+    company: "Miles McGraph",
+
+    timezone: "Africa/Accra",
+
+    language: "en",
+
+    pagination: 25,
+
+    darkMode: false
+
+};
