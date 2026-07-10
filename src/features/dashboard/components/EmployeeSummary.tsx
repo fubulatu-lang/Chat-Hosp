b4bc4dec8a1ac0002f4806edc557cf218e3c923f
fubulatu-lang <fubulatu@gmail.com>
2026-07-10 00:55:46 +0000

    Create src/features/dashboard/components/EmployeeSummary.tsx

---
+++ src/features/dashboard/components/EmployeeSummary.tsx
@@ -0,0 +1,23 @@
+import { Typography } from "@mui/material";
+
+import AppCard from "../../../components/ui/AppCard";
+
+export default function EmployeeSummary() {
+
+    return (
+
+        <AppCard>
+
+            <Typography variant="h6">
+                Employees
+            </Typography>
+
+            <Typography variant="h3">
+                0
+            </Typography>
+
+        </AppCard>
+
+    );
+
+}
